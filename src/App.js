47421b372bce211f6f0fb924c54949acd1555967
Azam18 <azam1995@mail.ru>
2019-04-20 20:35:53 -0400

    i tried something lol - react.js is hard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Button, Form, FormControl, Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
@@ -10,16 +10,16 @@ import { LinkContainer } from "react-router-bootstrap";
 class App extends Component {
 render() {
   return (
-    <div className="App container">
+	<div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Protobot</Link>
+         <Navbar.Brand>
+            <Link to="/">ProtoBot</Link>
           </Navbar.Brand>
-          <Navbar.Toggle />
+          <Navbar.Toggle aria-controls="justify-content-end" />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav pullRight>
+          <Nav pullLeft>
             <LinkContainer to="/signup">
               <NavItem>Signup</NavItem>
             </LinkContainer>
@@ -30,10 +30,16 @@ render() {
             <NavItem>Data</NavItem>
             </LinkContainer>
           </Nav>
+		  <Form inline>
+			<FormControl type="type" placeholder="Search" className="mr-sm-2" />
+			<Button variant="outline-success" className="btn">Search</Button>
+		  </Form>
         </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
+	
+    
   );
 }
 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./Home.css";
+import './template.jpg';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="Home">
         <div className="lander">
-          <h1>Protobot</h1>
+          <h1>ProtoBot</h1>
           <p>Crypto Pricing Resource</p>
         </div>
       </div>
