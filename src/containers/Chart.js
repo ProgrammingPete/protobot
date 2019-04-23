013af9a57c621c4d0cb94097375546af8a83c9e5
TonyD42 <Tony42D@gmail.com>
2019-04-23 01:06:03 -0400

    Candlestick chart added, candles still needs work

--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -2,44 +2,56 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { scaleTime } from "d3-scale";
-import { utcDay } from "d3-time";
+import { format } from "d3-format";
 
 import { ChartCanvas, Chart } from "react-stockcharts";
-import { CandlestickSeries } from "react-stockcharts/lib/series";
+import { BarSeries, CandlestickSeries } from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
+
+import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
 import { fitWidth } from "react-stockcharts/lib/helper";
-import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
+import { last } from "react-stockcharts/lib/utils";
 
 class CandleStickChart extends React.Component {
 	render() {
-		const { type, width, data, ratio } = this.props;
-		const xAccessor = d => d.Open_Time;
-		const xExtents = [
-			xAccessor(last(data)),
-			xAccessor(data[data.length - 100])
-		];
+		const { type, data: initialData, width, ratio } = this.props;
+
+		const xScaleProvider = discontinuousTimeScaleProvider
+			.inputDateAccessor(d => d.Open_Time);
+		const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(initialData);
+
+		const start = xAccessor(last(data));
+		const end = xAccessor(data[Math.max(0, data.length - 100)]);
+		const xExtents = [start, end];
+
 		return (
 			<ChartCanvas height={400}
-					ratio={ratio}
-					width={width}
-					margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
-					type={type}
-					seriesName="BTCUSDT"
-					data={data}
-					xAccessor={xAccessor}
-					xScale={scaleTime()}
-					xExtents={xExtents}>
+				ratio={ratio}
+				width={width}
+				margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+				type={type}
+				seriesName="BTCUSDT"
+				data={data}
+				xScale={xScale}
+				xAccessor={xAccessor}
+				displayXAccessor={displayXAccessor}
+				xExtents={xExtents}
+			>
 
-				<Chart id={1} yExtents={d => [d.High, d.Low]}>
-					<XAxis axisAt="bottom" orient="bottom" ticks={6}/>
-					<YAxis axisAt="left" orient="left" ticks={5} />
-					<CandlestickSeries width={timeIntervalBarWidth(utcDay)}/>
+				<Chart id={1} yExtents={d => [d.Open_Price, d.Close_Price]}>
+					<XAxis axisAt="bottom" orient="bottom"/>
+					<YAxis axisAt="right" orient="right" label="Price" ticks={5} />
+					<CandlestickSeries />
+				</Chart>
+				<Chart id={2} yExtents={d => d.Volume}>
+					<YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")}/>
+					<BarSeries yAccessor={d => d.Volume} />
 				</Chart>
 			</ChartCanvas>
 		);
 	}
 }
+
 
 CandleStickChart.propTypes = {
 	data: PropTypes.array.isRequired,
--- src/containers/Utils.js
+++ src/containers/Utils.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import { csvParse } from  "d3-dsv";
 import { timeParse, timeFormat } from "d3-time-format";
 
-function parseData(parse, format) {
+function parseData(parse) {
 	return function(d) {
 		d.index = +d.index;
 		d.Close_Price = +d.Close_Price;
-		format(d.Close_time);
 		d.Close_time = parse(d.Close_time);
 		d.High = +d.High;
 		d.Low = +d.Low;
 		d.Open_Price = +d.Open_Price;
-		format(d.Open_Time);
 		d.Open_Time = parse(d.Open_Time);
 		d.Volume = +d.Volume;
 
@@ -19,12 +17,11 @@ function parseData(parse, format) {
 	};
 }
 
-const parseDate = timeParse("%Y/%m/%d");
-const formatDate = timeFormat("%Y/%m/%d");
+const parseDate = timeParse("%Y/%m/%d %H:%M:%S");
 
 export function getTradingData(){
 	const tradingData = fetch("https://protoserver.centralus.cloudapp.azure.com/api/v1.0/btcOneWeek")
 	.then(response => response.text())
-	.then(data => csvParse(data, parseData(parseDate, formatDate)))
+	.then(data => csvParse(data, parseData(parseDate)))
 return tradingData;
 }
